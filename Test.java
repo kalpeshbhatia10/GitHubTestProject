e984608bd37f943b50a4d2e4751a5eee80860945
kalpeshbhatia10 <73269571+kalpeshbhatia10@users.noreply.github.com>
2020-10-30 14:24:06 -0700

    This commit is for release notes. Commit 2

--- Test.java
+++ Test.java
@@ -1,5 +1,5 @@
 public class Test {
-  // This commit is for release notes. Commit 1.
+  // This commit is for release notes. Commit 2.
   public static void main(String[] args) {
     System.out.println("Hello World, Java app");
   }
